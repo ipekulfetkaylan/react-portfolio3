ae5164acd5b462d28e8ece81a3ccaa12f79c6827
İpek Ülfet Kaylan <ipekulfetkaylan@gmail.com>
2023-05-21 23:56:42 +0300

    first commit

---
+++ src/components/about/About.jsx
@@ -0,0 +1,39 @@
+import "./about.css";
+
+function About() {
+  return (
+    <div className="a">
+      <div className="a-left">
+        <div className="a-card bg"></div>
+        <div className="a-card">
+          <img
+            src="https://images.pexels.com/photos/7034717/pexels-photo-7034717.jpeg?auto=compress&cs=tinysrgb&w=1600"
+            alt=""
+            className="a-img"
+          />
+        </div>
+      </div>
+      <div className="a-right">
+        <h1 className="a-title">About Me</h1>
+        <p className="sub-title">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima
+          explicabo beatae quasi quae, tenetur dolor ab? A minus quam sint.
+        </p>
+        <p className="a-desc">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, expedita
+          aut dolore assumenda iste eos molestias doloribus deserunt provident?
+          A ullam labore, possimus cum omnis ex facilis distinctio animi,
+          eveniet quasi, earum eum iusto hic veniam. Obcaecati officia iure
+          doloremque, placeat amet deleniti atque enim nisi eius quis. Sequi
+          nostrum necessitatibus ea laboriosam nam, hic quas aliquid esse ut
+          eligendi?
+        </p>
+        <div className="a-award">
+            <img src="" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,35 @@
+import "./intro.css";
+import Man from "../../img/Womann.png";
+
+function Intro() {
+  return (
+    <div className="i">
+      <div className="i-left">
+        <div className="i-left-wrapper">
+          <h2 className="i-intro">Hello, My name is</h2>
+          <h1 className="i-name">John Doe</h1>
+          <div className="i-title">
+            <div className="i-title-wrapper">
+              <div className="i-title-item">Web Developer</div>
+              <div className="i-title-item">Front End Developer</div>
+              <div className="i-title-item">React</div>
+              <div className="i-title-item">Javascript</div>
+            </div>
+          </div>
+          <p className="i-desc">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam
+            blanditiis maxime obcaecati aliquid et. 
+            odit illum obcaecati, illo enim, quae voluptas blanditiis vitae?
+            Cupiditate ullam odio, corrupti architecto eum et?
+          </p>
+        </div>
+      </div>
+      <div className="i-right">
+        <div className="i-bg"></div>
+        <img src={Man} alt="" className="i-img" />
+      </div>
+    </div>
+  );
+}
+
+export default Intro;
